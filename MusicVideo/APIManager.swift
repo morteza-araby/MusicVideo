b0c603e7bad2161ffd94596e3276732245e07434
Morteza Araby <morteza.araby@outlook.com>
2016-09-12 15:13:03 +0200

    using NSJSONSerialization to serialise raw data to JSON

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -16,24 +16,48 @@ class APIManager {
         // in AppDelegate
         
         let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
-        
         let session = NSURLSession(configuration: config)
         
         //let session = NSURLSession.sharedSession()
         let url = NSURL(string: urlString)
         
+        
         let task = session.dataTaskWithURL(url!){
             (data, response, error) -> Void in
             
-            dispatch_async(dispatch_get_main_queue()) {
-                if error != nil {
+            if error != nil{
+                dispatch_async(dispatch_get_main_queue()) {
                     completion(result: (error!.localizedDescription))
-                } else {
-                    completion(result: "NSURLSession successful")
-                    print(data)
+                }
+            } else{
+                //Added for JSONSerialization
+                //print(data)
+                do {
+                    /* .AllowFragments - top level object is not Array or Dictionary.
+                     Any type of string or value
+                     NSJSONSerialization requires the Do / Try / Catch
+                     Converts the NSDATA into a JSON Object and cast it to a Dictionary
+                     */
+                    if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+                        as? [String: AnyObject] {
+                        print(json)
+                        
+                        let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+                            dispatch_async(dispatch_get_main_queue()) {
+                                completion(result: "JSONSerialization Successful")
+                            }
+                        }
+                    }
+                    
+                } catch {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        completion(result: "error in NSJSONSerialization")
+                    }
                 }
             }
         }
+     
         //Start the task, task by default is suspended
         task.resume()
     }
